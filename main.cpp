d67681d66a4607dd4b44ace3160dcc2940ad9663
nizihad824 <mdnurulislam97@gmail.com>
2022-02-24 10:10:10 +0100

    class SimError and Patient

--- main.cpp
+++ main.cpp
@@ -16,3 +16,28 @@ ostream& operator <<(ostream& out ,Vaccine& v){
         return out<<"none";
     }
 }
+
+//  class SimError 
+class SimError:public exception {
+public:
+    string reason;
+    SimError(string r):
+        reason(r)
+    {}
+};
+// class Patient
+class Patient{
+private:
+    int no;
+    const int id;
+    bool briefed;
+    Vaccine vaccine;
+
+public:
+    Patient();
+    int get_id();
+    bool is_briefed();
+    void switch_briefed();
+    void set_vaccinated(Vaccine);
+    friend ostream& operator<<(ostream&, Patient&);
+};
