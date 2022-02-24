2c4afbb58f5b09b8a09fd9942c824f2489db4a5e
nizihad824 <mdnurulislam97@gmail.com>
2022-02-24 10:07:29 +0100

    output operator

--- main.cpp
+++ main.cpp
@@ -1,2 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
+enum Vaccine {astra,biontech,moderna,none};
+// output operator 
+ostream& operator <<(ostream& out ,Vaccine& v){
+   switch(v)
+    {
+    case astra:
+        return out<<"AstraZeneca";
+    case biontech:
+        return out<<"BioNTech";
+
+    case moderna:
+        return out<<"Moderna";
+    case none:
+        return out<<"none";
+    }
+}
